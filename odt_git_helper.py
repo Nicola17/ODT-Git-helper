1eb71f4f745c27d4b9575760d397c94649abf140
Nicola Pezzotti <nicola.pezzotti@gmail.com>
2014-02-07 23:14:13 +0100

    Adds odt_git_helper.py

---
+++ odt_git_helper.py
@@ -0,0 +1,51 @@
+import os
+import zipfile
+import argparse
+
+def createODT(path, zipName, args):
+    zipf = zipfile.ZipFile(zipName, 'w')
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if 'Thumbs.db' in file:
+                continue
+            zipf.write(os.path.join(root, file), os.path.join(root, file)[path.__len__():])
+            if args.verbose:
+                print os.path.join(root, file)[path.__len__():]
+        for dir in dirs:
+            zipf.write(os.path.join(root, dir), os.path.join(root, dir)[path.__len__():])
+            if args.verbose:
+                print os.path.join(root, dir)[path.__len__():]
+
+    zipf.close()
+
+def extractODT(path, zipName, args):
+    zipf = zipfile.ZipFile(zipName,'r')
+    for name in zipf.namelist():
+        outpath = "C:\\"
+        zipf.extract(name, path)
+    zipf.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract/Create an odt file in/from a directory.')
+    
+    parser.add_argument("-v","--verbose", action='store_true', default=False, help="increase output verbosity")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-c", "--compress", action="store_true", default=False, help='compress into an .odt file')
+    group.add_argument("-e", "--extract", action="store_true", default=False, help='extract odt file into a directory (default)')
+
+    parser.add_argument('directory', metavar='Directory', type=str, help='target/destination directory')
+    parser.add_argument('odt_file', metavar='ODT_File', type=str, help='target/destination odt file')
+
+    args = parser.parse_args()
+    
+    if args.extract or (not args.extract and not args.compress):
+        print 'Extract files'
+        extractODT(args.directory, args.odt_file, args)
+
+    if args.compress:
+        print 'Compress files'
+        createODT(args.directory, args.odt_file, args)
+        
+
+    

ca0c01bb6d7e768e7ef6e744220102872c534cac
Nicola Pezzotti <nicola.pezzotti@gmail.com>
2014-02-08 02:08:13 +0100

    Adds an xml processing in order make it more readable for Git

--- odt_git_helper.py
+++ odt_git_helper.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import argparse
+import xml.dom.minidom
 
 def createODT(path, zipName, args):
     zipf = zipfile.ZipFile(zipName, 'w')
@@ -11,6 +12,7 @@ def createODT(path, zipName, args):
             zipf.write(os.path.join(root, file), os.path.join(root, file)[path.__len__():])
             if args.verbose:
                 print os.path.join(root, file)[path.__len__():]
+
         for dir in dirs:
             zipf.write(os.path.join(root, dir), os.path.join(root, dir)[path.__len__():])
             if args.verbose:
@@ -21,13 +23,33 @@ def createODT(path, zipName, args):
 def extractODT(path, zipName, args):
     zipf = zipfile.ZipFile(zipName,'r')
     for name in zipf.namelist():
-        outpath = "C:\\"
         zipf.extract(name, path)
     zipf.close()
 
+    if args.verbose:
+        print "Pretty xml generation"
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            fileWPath = os.path.join(root, file)
+            if fileWPath[-3:] == "xml":
+                if args.verbose:
+                    print fileWPath
+
+                statinfo = os.stat(fileWPath)
+                if statinfo.st_size == 0:
+                    continue
+
+                xmlFile = xml.dom.minidom.parse(fileWPath)
+                pretty_xml_as_string = xmlFile.toprettyxml()
+
+                out_file = open(fileWPath,"w")
+                out_file.write(pretty_xml_as_string)
+                out_file.close()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract/Create an odt file in/from a directory.')
-    
+
     parser.add_argument("-v","--verbose", action='store_true', default=False, help="increase output verbosity")
 
     group = parser.add_mutually_exclusive_group()
@@ -38,7 +60,7 @@ if __name__ == '__main__':
     parser.add_argument('odt_file', metavar='ODT_File', type=str, help='target/destination odt file')
 
     args = parser.parse_args()
-    
+
     if args.extract or (not args.extract and not args.compress):
         print 'Extract files'
         extractODT(args.directory, args.odt_file, args)
@@ -46,6 +68,5 @@ if __name__ == '__main__':
     if args.compress:
         print 'Compress files'
         createODT(args.directory, args.odt_file, args)
-        
 
-    
+
